a85823df179fd6a93f13a283d655f75b80a1fac3
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2019-11-17 12:42:34 -0600

    test: Add end-to-end test

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,9 +1,45 @@
-context('CRA test', () => {
+context('TODO test', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
-  it('Should show the link', () => {
-    cy.findByText(/Learn React/).should('be.visible');
+  it('should show the title', () => {
+    cy.findByText(/TODO APP/).should('be.visible');
+  });
+
+  it('should create a todo', () => {
+    cy.findByRole('textbox')
+      .type('Make a sandwich')
+      .findByRole('button')
+      .click();
+
+    cy.findByRole('list').should('have.length', 1);
+  });
+
+  it('should remove a todo', () => {
+    cy.findByRole('textbox')
+      .type('Make a sandwich{enter}')
+      .findByRole('list')
+      .should('have.length', 1)
+      .findByLabelText(/Delete todo/)
+      .click();
+
+    cy.findByRole('list')
+      .its('length')
+      .should('be', 0);
+  });
+
+  it('should toggle a todo', () => {
+    cy.findByRole('textbox')
+      .type('Make a sandwich')
+      .findByRole('button')
+      .click()
+      .findByRole('checkbox')
+      .click();
+
+    cy.findByRole('list')
+      .should('have.length', 1)
+      .findByRole('checkbox')
+      .should('be.checked');
   });
 });
