d89b8a20e7d078dc102927189c34f48bb3aeab6c
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-09-15 13:03:58 -0600

    chore(storybook): Fix Storybook setup

--- .storybook/config.ts
+++
@@ -1,4 +0,0 @@
-import { configure } from '@storybook/react';
-
-// automatically import all files ending in *.stories.(tsx|jsx)
-configure(require.context('../src', true, /\.stories\.(mdx|[tj]sx?)$/), module);
